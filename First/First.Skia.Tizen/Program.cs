8ef426b83ad51b8d7d29bb272dfeb19755576130
Damola Omotosho <damola.omotosho@gmail.com>
2021-04-18 09:45:17 -0700

    Initial commit

---
+++ First/First.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace First.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new First.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ First/First.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace First.Skia.Gtk
+{
+}
